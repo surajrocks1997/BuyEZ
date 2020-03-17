8f67fff1a8131b69c3f67929be443171a822f500
surajrocks1997 <surajrocks1997@gmail.com>
2020-03-17 14:54:08 +0530

    first commit, homescreen and theming added

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BuyEZ',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
